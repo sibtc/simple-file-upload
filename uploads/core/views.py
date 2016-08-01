ae135e0a6b160c01af8685aa658c95fed37ac0ce
Vitor Freitas <vitorfs@gmail.com>
2016-08-01 15:36:41 +0300

    Updated to use FileSystemStorage

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 from uploads.core.models import Document
 from uploads.core.forms import DocumentForm
@@ -15,13 +16,13 @@ def simple_upload(request):
         if request.FILES['myfile']:
             myfile = request.FILES['myfile']
 
-            path = u'{0}/{1}'.format(settings.MEDIA_ROOT, myfile.name)
-            with open(path, 'wb+') as destination:
-                for chunk in myfile.chunks():
-                    destination.write(chunk)
+            fs = FileSystemStorage()
+            up = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(myfile.name)
 
-            uploaded_file_url = u'{0}{1}'.format(settings.MEDIA_URL, myfile.name)
-            return render(request, 'core/simple_upload.html', { 'uploaded_file_url': uploaded_file_url })
+            return render(request, 'core/simple_upload.html', {
+                'uploaded_file_url': uploaded_file_url
+            })
 
     return render(request, 'core/simple_upload.html')
 
