b50ae7dcd40b96eff6a4b0200a16100d0cfd06cb
Vitor Freitas <vitorfs@gmail.com>
2016-08-01 16:16:42 +0300

    .

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -15,8 +15,8 @@ def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
-        fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(myfile.name)
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
         return render(request, 'core/simple_upload.html', {
             'uploaded_file_url': uploaded_file_url
         })
